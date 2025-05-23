a8da504488556bd58aa7443a4f6804b3e6aec76a
eemils01 <eeglite17@gmail.com>
2025-05-23 20:04:57 +0000

    a

---
+++ main.js
@@ -0,0 +1,15 @@
+function show(){
+ let sk=document.getElementById("fname").value;
+ console.log(typeof sk);
+ let kvadrats=sk+sk;
+ document.getElementById("izvads").innerHTML=kvadrats;
+}
+
+
+
+
+
+
+
+
+
